1a32746c2c2cb88b978e3cd16bab52fd708219fe
Duong Quang Minh <s3759757@student.rmit.edu.au>
2023-08-10 14:38:09 +1000

    Revert "register logic done"
    This reverts commit b2f46fe3208ee1c5304cbd5eab6a97dc82a4ca5b.

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -9,10 +9,6 @@ from .serializers import TodoSerializer
 # import the Todo model from the models file
 from .models import Todo
 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .forms import NewUserForm
 # create a class for the Todo model viewsets
 class TodoView(viewsets.ModelViewSet):
 
@@ -23,12 +19,3 @@ class TodoView(viewsets.ModelViewSet):
 	# define a variable and populate it
 	# with the Todo list objects
 	queryset = Todo.objects.all()
-
-@api_view(['POST'])
-def register(request):
-    if request.method == 'POST':
-        form = NewUserForm(request.data)
-        if form.is_valid():
-            user = form.save()
-            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
-        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
