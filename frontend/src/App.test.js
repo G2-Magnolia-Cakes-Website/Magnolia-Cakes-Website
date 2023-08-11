73016058528208b4fb4b1499dc5347f3cc6ce70d
Kim Ta <kimt12531@gmail.com>
2023-08-11 11:12:11 +1000

    Update tests and styling.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,12 +1,19 @@
 // import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders learn react link", () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 
-  const wrapper = renderer.create(<App />).toJSON();
+  const wrapper = renderer
+    .create(
+      // <BrowserRouter>
+      <App />
+      // </BrowserRouter>
+    )
+    .toJSON();
   expect(wrapper).toMatchSnapshot();
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 // importing css stylesheet to use the bootstrap class
 // add this line only in this file
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+reportWebVitals();
