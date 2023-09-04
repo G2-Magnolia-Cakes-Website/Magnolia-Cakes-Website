ab58ea5931063e1036f967a75ec116666217f06d
Kim Ta <kimt12531@gmail.com>
2023-09-04 17:09:49 +1000

    Image upload and local storage by admin via backend app.

--- backend/MagnoliaCakesAndCupcakes/models.py
+++ backend/MagnoliaCakesAndCupcakes/models.py
@@ -1,14 +1,19 @@
 from django.db import models
- 
+
+
 class MagnoliaCakesAndCupcakes(models.Model):
-    title=models.CharField(max_length=150)
- 
+    title = models.CharField(max_length=150)
+
     # string representation of the class
     def __str__(self):
- 
-        #it will return the title
+        # it will return the title
         return self.title
+
 
 class TermsAndConditions(models.Model):
     content = models.TextField()
     last_updated = models.DateTimeField(auto_now=True)
+
+
+class Gallery(models.Model):
+    gallery_image = models.ImageField(null=False, blank=False, upload_to="images/")
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,22 +10,32 @@ from MagnoliaCakesAndCupcakes import views
 # it is necessary for routing
 from rest_framework import routers
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # create a router object
 router = routers.DefaultRouter()
 
 # register the router
-router.register(r'MagnoliaCakesAndCupcakes',views.MagnoliaCakesAndCupcakesView, 'MagnoliaCakesAndCupcakes')
+router.register(
+    r"MagnoliaCakesAndCupcakes",
+    views.MagnoliaCakesAndCupcakesView,
+    "MagnoliaCakesAndCupcakes",
+)
 
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-
-	# add another path to the url patterns
-	# when you visit the localhost:8000/api
-	# you should be routed to the django Rest framework
-	path('api/', include(router.urls)),
-	path('api/register/', views.register, name='api-register'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-	path('api/terms-and-conditions/', views.terms_and_conditions, name='terms-and-conditions'),
-	path('api/login/', views.login, name='api-login'),
-]
+    path("admin/", admin.site.urls),
+    # add another path to the url patterns
+    # when you visit the localhost:8000/api
+    # you should be routed to the django Rest framework
+    path("api/", include(router.urls)),
+    path("api/register/", views.register, name="api-register"),
+    path("activate/<uidb64>/<token>", views.activate, name="activate"),
+    path(
+        "api/terms-and-conditions/",
+        views.terms_and_conditions,
+        name="terms-and-conditions",
+    ),
+    path("api/login/", views.login, name="api-login"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
