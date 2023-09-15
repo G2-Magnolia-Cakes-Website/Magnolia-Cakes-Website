edc5ffa8097cd09e62157ef1f679bfd83b2feb65
Omer Yalavac <s3849677@student.rmit.edu.au>
2023-09-15 13:59:36 +1000

    Undo venv changes

--- dar/Scripts/activate.bat
+++ dar/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Omer Yalavac\Desktop\Capstone Project\Magnolia-Cakes-Website\dar"
+set VIRTUAL_ENV=C:\Users\nucle\Documents\IT_Master\Sem_3\Capstone\Magnolia-Cakes-Website\django-todo-react\dar
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -19,7 +19,7 @@ set _OLD_VIRTUAL_PROMPT=%PROMPT%
 set PROMPT=(dar) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
-set "PYTHONHOME=C:\Users\Omer Yalavac\AppData\Local\Programs\Python\Python311"
+set PYTHONHOME=
 
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
