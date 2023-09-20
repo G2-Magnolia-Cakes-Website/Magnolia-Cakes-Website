18a128de983b209d71856e42cd53f10850aae393
Kim Ta <kimt12531@gmail.com>
2023-09-20 20:45:34 +1000

    Fix imports for ForgotPasswordPage and ResetPasswordPage

--- frontend/src/Components/ForgotPassword/ResetPasswordPage.js
+++ frontend/src/Components/ForgotPassword/ResetPasswordPage.js
@@ -1,36 +1,34 @@
-import './ForgotPassword.css';
+import "./ForgotPassword.css";
 import Form from "./ResetPasswordForm";
-import magnoliaCakeLogo from "../../utils/Magnolia_Cake_logo.png";
-import birthdayCake from "../../utils/carousel-wedding-ann.jpg";
-import React from 'react';
+import magnoliaCakeLogo from "utils/Magnolia_Cake_logo.png";
+import birthdayCake from "utils/wedding-ann.jpg";
+import React from "react";
 
 function ResetPasswordPage({ api }) {
-    const logo = (
-        <img className="logo" src={magnoliaCakeLogo} alt="Magnolia Cake Logo" />
-    );
+  const logo = (
+    <img className="logo" src={magnoliaCakeLogo} alt="Magnolia Cake Logo" />
+  );
 
-    const image = (
-        <img className='login-image' src={birthdayCake} alt='Cake Image' />
-    )
+  const image = (
+    <img className="login-image" src={birthdayCake} alt="Cake Image" />
+  );
 
-    return (
-        <div className='white-background'>
-            <div className="loginPage">
-                <div className='login-form'>
-                    <div className='centre-form'>
-                        <div className='logo-div'>
-                            {logo}
-                        </div>
-                        <div>
-                            <h1 className='login-header'>Enter Your new Password</h1>
-                        </div>
-                        <Form api={api} />
-                    </div>
-                </div>
-                {image}
+  return (
+    <div className="white-background">
+      <div className="loginPage">
+        <div className="login-form">
+          <div className="centre-form">
+            <div className="logo-div">{logo}</div>
+            <div>
+              <h1 className="login-header">Enter Your new Password</h1>
             </div>
+            <Form api={api} />
+          </div>
         </div>
-    );
+        {image}
+      </div>
+    </div>
+  );
 }
 
 export default ResetPasswordPage;
