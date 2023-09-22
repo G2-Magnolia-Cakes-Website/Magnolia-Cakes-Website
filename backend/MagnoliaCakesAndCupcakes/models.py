7156ccf655a8d0bb643545ae64f6eff68c2222f4
Kim Ta <kimt12531@gmail.com>
2023-09-22 13:22:16 +1000

    Populate database with footer content

--- backend/MagnoliaCakesAndCupcakes/models.py
+++ backend/MagnoliaCakesAndCupcakes/models.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from django.core.mail import EmailMessage
 from django_rest_passwordreset.signals import reset_password_token_created
 
+
 class MagnoliaCakesAndCupcakes(models.Model):
     title = models.CharField(max_length=150)
 
@@ -19,13 +20,15 @@ class MagnoliaCakesAndCupcakes(models.Model):
 
 
 class TermsAndCondition(models.Model):
-    policy_name =  models.CharField(max_length=100)
+    policy_name = models.CharField(max_length=100)
     policy_content = models.TextField()
+
     class Meta:
         ordering = ["policy_name"]
 
     def __str__(self):
         return self.policy_name
+
 
 def upload_to(instance, filename):
     # Upload the image to a 'cakes' directory with the filename as the cake's name
@@ -136,6 +139,8 @@ class FooterBusinessHours(models.Model):
 
     def __str__(self):
         return "Business Hours"
+
+
 class FlavoursAndServings(models.Model):
     title = models.CharField(max_length=100)
     list = models.TextField()
@@ -185,21 +190,24 @@ class SocialMedias(models.Model):
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-
+def password_reset_token_created(
+    sender, instance, reset_password_token, *args, **kwargs
+):
     # send an e-mail to the user
-    reset_password_link = f"{settings.FRONTEND_APP_URL}/reset-password/?token={reset_password_token.key}"
-    
+    reset_password_link = (
+        f"{settings.FRONTEND_APP_URL}/reset-password/?token={reset_password_token.key}"
+    )
+
     context = {
-        'current_user': reset_password_token.user,
-        'firstname': reset_password_token.user.first_name,
-        'lastname': reset_password_token.user.last_name,
-        'email': reset_password_token.user.email,
-        'reset_password_link': reset_password_link
+        "current_user": reset_password_token.user,
+        "firstname": reset_password_token.user.first_name,
+        "lastname": reset_password_token.user.last_name,
+        "email": reset_password_token.user.email,
+        "reset_password_link": reset_password_link,
     }
 
     # render email text
-    email_message = render_to_string('user_reset_password.html', context)
+    email_message = render_to_string("user_reset_password.html", context)
 
     mail_subject = "Password Reset for {title}".format(title="Magnolia Cakes")
 
@@ -210,7 +218,6 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         # from:
         settings.EMAIL_FROM,
         # to:
-        [reset_password_token.user.email]
+        [reset_password_token.user.email],
     )
     msg.send()
-
