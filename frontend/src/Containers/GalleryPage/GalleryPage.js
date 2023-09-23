dd25b8186d70a1289fcba26d973cb7a4f9739848
Duong Quang Minh <s3759757@student.rmit.edu.au>
2023-09-23 21:29:28 +1000

    frontend finished

--- frontend/src/Containers/GalleryPage/GalleryPage.js
+++ frontend/src/Containers/GalleryPage/GalleryPage.js
@@ -1,60 +1,68 @@
 import React, { useState, useEffect } from 'react';
+import './GalleryPage.css';
 
-
-const GalleryPage = ({api}) => {
+const GalleryPage = ({ api }) => {
+  const all = { id: -1, name: 'All' };  // Corrected 'name' property here
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [selectedCategory, setSelectedCategory] = useState(all);
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     // Fetch gallery categories
     api.get('/api/gallery/categories/')
       .then(response => {
-        setCategories(['all', ...response.data.map(category => category.name)]); // Extract category names
+        setCategories([all, ...response.data]);  // Include 'all' as the first category
+        handleCategoryChange(all);  // Set default category to 'all'
       })
       .catch(error => console.error('Error fetching categories:', error));
-  }, []);
+  }, [api]);
 
-  const handleCategoryChange = (category) => {
+  const handleCategoryChange = category => {
     setSelectedCategory(category);
-    if (category === 'all') {
-      // Fetch all images
-      api.get('/api/gallery/items/')
-        .then(response => {
-          setImages(response.data);
-        })
-        .catch(error => console.error('Error fetching images:', error));
+    let endpoint;
+    if (category.id === all.id) {
+      endpoint = '/api/gallery/items/';
     } else {
-      // Fetch images for the selected category
-      api.get(`/api/gallery/items/?category=${category}`)
-        .then(response => {
-          setImages(response.data);
-        })
-        .catch(error => console.error('Error fetching images:', error));
+      endpoint = `/api/gallery/items/?category=${category.id}`;
     }
+  
+    api.get(endpoint)
+      .then(response => {
+        setImages(response.data);
+      })
+      .catch(error => console.error('Error fetching images:', error));
   };
 
-  return (
-    <div>
-      <h1>Gallery</h1>
-      <label htmlFor="category">Select a category: </label>
-      <select
-        id="category"
-        value={selectedCategory}
-        onChange={(e) => handleCategoryChange(e.target.value)}
-      >
-        {categories.map(category => (
-          <option key={category} value={category}>{category}</option>
-        ))}
-      </select>
+  const filteredImages = selectedCategory.id === all.id
+    ? images
+    : images.filter(image => image.categories.includes(selectedCategory.id));
 
-      <div className="image-grid">
-        {images.map(image => (
-          <img key={image.id} src={image.image} alt={image.title} />
-        ))}
+    return (
+      <div className="GalleryPage">
+        <h1 className="PageHeader">Gallery</h1>
+        <div className="GalleryCategory">
+          {categories.map(category => (
+            <button
+              key={category.id}
+              className={selectedCategory.id === category.id ? 'selected-category' : 'category'}
+              onClick={() => handleCategoryChange(category)}
+            >
+              {category.name.toUpperCase()}
+            </button>
+          ))}
+        </div>
+  
+        <div className="image-grid">
+          {filteredImages.map(image => (
+            <img
+              key={image.id}
+              src={image.image}
+              alt={image.title}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default GalleryPage;
