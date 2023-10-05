d128c6529c891d8c0aec037f439bbf1f6eb0e407
Duong Quang Minh <s3759757@student.rmit.edu.au>
2023-10-05 14:03:51 +1100

    add cloud url to checkout

--- backend/MagnoliaCakesAndCupcakes/views.py
+++ backend/MagnoliaCakesAndCupcakes/views.py
@@ -477,6 +477,7 @@ def location_page_content(request):
 def create_checkout_session(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     FRONTEND_DOMAIN = "http://localhost:3000"
+    # FRONTEND_DOMAIN = "https://alpine-avatar-399423.ts.r.appspot.com/"
     
     # Get the cart items from the request
     cart_items = request.data.get('items', [])
