418fcc5ab82968ac3baf20e09085caa8787ecfbf
Omer Yalavac <s3849677@student.rmit.edu.au>
2023-10-06 23:35:05 +1100

    Frontend Promotion popup

--- frontend/src/Components/PromotionPopup/PromotionPopup.js
+++ frontend/src/Components/PromotionPopup/PromotionPopup.js
@@ -1,34 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
+import './PromotionPopup.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
-const Popup = ({api}) => {
-  const [showPopup, setShowPopup] = useState(false);
+const Popup = ({ api }) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const [promotion, setPromotion] = useState(null);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  useEffect(() => {
-    const hasPopupShownBefore = Cookies.get('popupShown');
+    // Get promotion
+    useEffect(() => {
+        // Make a GET request using the passed api instance
+        api.get('/api/promotions/displayed/')
+            .then(response => {
+                // Set the retrieved content in the state
+                if (response.status === 200) {
+                    setPromotion(response.data);
+                } else {
+                    console.log('No promotion found.')
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
 
-    if (!hasPopupShownBefore) {
-      setShowPopup(true);
-      Cookies.set('popupShown', 'true'); 
+    // Show popup
+    useEffect(() => {
+        const hasPopupShownBefore = Cookies.get('popupShown');
+
+        if (!hasPopupShownBefore) {
+            const timeout = setTimeout(() => {
+                setShowPopup(true);
+                Cookies.set('popupShown', 'true', { expires: 1 });
+            }, 30000); // Delay of 30 seconds (30000 milliseconds)
+
+            return () => clearTimeout(timeout); // Clear the timeout if the component unmounts before the delay
+        }
+    }, [promotion]);
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(promotion.code);
+        // show a success message or perform any other desired action after copying to the clipboard
+        setShowSuccessMessage(true);
+        // set a timeout to hide the message after a certain duration
+        setTimeout(() => {
+            setShowSuccessMessage(false);
+        }, 3000); // Hide the message after 3 seconds 
+    };
+
+    // Render only if there is a promotion
+    if (!showPopup || !promotion) {
+      return null;
     }
-  }, []);
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
-
-  return (
-    showPopup && (
-      <div className="popup">
-        {/* Popup content */}
-        <div className="popup-content">
-          <h3>Popup Title</h3>
-          <p>Popup content goes here...</p>
-          <button onClick={handleClosePopup}>Close</button>
-        </div>
-      </div>
-    )
-  );
+    return (
+        showPopup && (
+            <>
+                {showSuccessMessage && <div className="success-message">Copied to clipboard!</div>}
+                <div className='promotion-popup'>
+                    <div className='promotion-popup-inner'>
+                        <button className='close-btn' onClick={handleClosePopup}>X</button>
+                        {/* Popup content */}
+                        <div className="promotion-popup-content">
+                            <h3>PROMOTION!</h3>
+                            <div className="promotion-code-container">
+                                <div className="promotion-code">{promotion.code}</div>
+                                <button className="copy-btn" onClick={copyToClipboard}>
+                                    <FontAwesomeIcon icon={faCopy} />
+                                </button>
+                            </div>
+                            <p>{promotion.description}</p>
+                            <button className='yes-btn-welcome' onClick={handleClosePopup}>Thanks!</button>
+                        </div>
+                    </div>
+                </div >
+            </>
+        )
+    );
 };
 
 export default Popup;
